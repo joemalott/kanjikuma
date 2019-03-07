969ebf454010547f649902837ca09583d252ebd3
Joe <joe@Joes-MacBook-Pro.local>
2019-03-07 09:02:13 +0900

    Converted to just theme instead of full sqlitepress installation

--- index.php
+++ index.php
@@ -1,17 +1,73 @@
+<?php get_header(); ?>
 <?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+	$loop = new WP_Query( array(
+	'post_type' => 'Radical',
+	'posts_per_page' => -1,
+	'orderby'   => 'meta_value_num',
+		'meta_key'  => 'radical_options_stroke_count',
+	'order'     => 'ASC',
+	)
+	);
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
+	$previous = " ";
+?>
+<body>
+<div class="container">
+	<div class="component">
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+		<div class="title">
+			<img src="<?php echo get_stylesheet_directory_uri() . "/svg/001-bear.svg" ?>" width="50%" style="margin: 3rem auto 0 auto; display: block;">
+			<h2 style="margin: 0;padding-top: 1rem;">Kanji Kuma</h2>
+			<p>From <a class="title-link" href="http://joemalott.com">Joe Malott</a></p>
+		</div>
+
+		<ul class="grid">
+			<?php
+
+				// 'red', 'blue', 'green', 'yellow', 'purple', 'pink'
+
+				if ( $loop->have_posts() ) :
+				while ( $loop->have_posts() ) : $loop->the_post();
+					$items = ['left', 'top', 'right', 'bottom'];
+					$position = $items[array_rand($items)];
+
+					$colors = ['red', 'blue', 'green', 'yellow', 'pink', 'salmon', 'fuschia', 'grey', 'aqua', 'coral'];
+					$color = $colors[array_rand($colors)];
+					
+					if ($previous == $color) {
+						$colors = ['red', 'blue', 'green', 'yellow', 'pink', 'salmon', 'fuschia', 'grey', 'aqua', 'coral'];
+						$color = $colors[array_rand($colors)];
+
+						if ($previous == $color) {
+							$colors = ['red', 'blue', 'green', 'yellow', 'pink', 'salmon', 'fuschia', 'grey', 'aqua', 'coral'];
+							$color = $colors[array_rand($colors)];
+
+							if ($previous == $color) {
+								$colors = ['red', 'blue', 'green', 'yellow', 'pink', 'salmon', 'fuschia', 'grey', 'aqua', 'coral'];
+								$color = $colors[array_rand($colors)];
+							} else {
+								$previous = $color;
+							}
+
+						} else {
+							$previous = $color;
+						}
+
+					} else {
+						$previous = $color;
+					}
+			?>
+			
+			<li class=<?php echo "\"ot-letter-" . $position . " " . $color . "\"";?>>
+				<a href="<?php echo esc_url( get_permalink() ); ?>">
+					<span data-letter="<?php echo esc_html( get_the_title() ); ?>"><?php echo esc_html( get_the_title() ); ?></span>
+				</a>
+			</li>
+			
+			<?php endwhile; wp_reset_query(); endif; ?>
+			
+		</ul>
+	</div>
+</div>
+</body>
+<?php get_footer(); ?>
