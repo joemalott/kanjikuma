9f446c397eb7e46bdbda712a38d4e6538162d37e
Joe Malott <joe@malott.net>
2019-03-07 01:05:56 +0000

    favicons

--- header.php
+++ header.php
@@ -6,5 +6,12 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 	<meta name="description" content="Kanji Kuma is a resource for learning Japanese Kanji radicals." />
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <link rel="manifest" href="/site.webmanifest">
+  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+  <meta name="msapplication-TileColor" content="#ffc40d">
+  <meta name="theme-color" content="#ffffff">
 	<?php wp_head(); ?>
 </head>
